70d7bc772c385eac6ad443f5ce0721d1244260de
Joakim Ribier <jribier@linagora.com>
2011-11-09 15:12:16 +0100

    - [OBMFULL-2724] remove BookType parameter of createContact and delete other createContact* ws

--- java/sync/services/src/main/java/fr/aliacom/obm/common/calendar/CalendarBindingImpl.java
+++ java/sync/services/src/main/java/fr/aliacom/obm/common/calendar/CalendarBindingImpl.java
@@ -508,7 +508,7 @@ public class CalendarBindingImpl implements ICalendar {
 		}
 	}
 	
-	private Event createEventAsDeleted(AccessToken token, String calendar, Event event) throws SQLException, FindException {
+	private Event createEventAsDeleted(AccessToken token, String calendar, Event event) throws SQLException, FindException, ServerFault {
 		Event ev = calendarDao.createEvent(token, calendar, event, false);
 		return calendarDao.removeEvent(token, ev, event.getType(), event.getSequence());
 	}
@@ -1145,6 +1145,8 @@ public class CalendarBindingImpl implements ICalendar {
 		} catch (FindException e) {
 			throw new ImportICalendarException(e);
 		} catch (SQLException e) {
+			throw new ImportICalendarException(e);
+		} catch (ServerFault e) {
 			throw new ImportICalendarException(e);
 		}
 		return false;
