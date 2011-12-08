c7c5f688e0de8de1cd3307bf0672982231c9284f
Matthieu Baechler <matthieu.baechler@linagora.com>
2011-12-08 14:16:01 +0100

    - [OBMFULL-2869] Move imap client creation into its own class to make it possible to   mock it in integration tests

--- java/sync/opush/imap/src/main/java/org/minig/imap/IdleClient.java
+++ java/sync/opush/imap/src/main/java/org/minig/imap/IdleClient.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IdleClient {
-
+	
 	private static final Logger logger = LoggerFactory
 			.getLogger(IdleClient.class);
 	
