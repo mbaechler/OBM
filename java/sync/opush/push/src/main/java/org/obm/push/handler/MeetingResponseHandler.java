5423edc44acee7508b8e024443bd31d6c70cee56
Matthieu Baechler <matthieu.baechler@linagora.com>
2011-12-16 18:13:49 +0100

    - [OBMFULL-2973] log server errors

--- java/sync/opush/push/src/main/java/org/obm/push/handler/MeetingResponseHandler.java
+++ java/sync/opush/push/src/main/java/org/obm/push/handler/MeetingResponseHandler.java
@@ -79,18 +79,19 @@ public class MeetingResponseHandler extends WbxmlRequestHandler {
 			sendResponse(responder, document);
 			
 		} catch (NoDocumentException e) {
-			sendErrorResponse(responder, MeetingResponseStatus.INVALID_MEETING_RREQUEST, e);
+			sendErrorResponse(responder, MeetingResponseStatus.INVALID_MEETING_RREQUEST);
 		} catch (DaoException e) {
-			sendErrorResponse(responder, MeetingResponseStatus.SERVER_ERROR, e);
+			logger.error(e.getMessage(), e);
+			sendErrorResponse(responder, MeetingResponseStatus.SERVER_ERROR);
 		} catch (CollectionNotFoundException e) {
-			sendErrorResponse(responder, MeetingResponseStatus.INVALID_MEETING_RREQUEST, e);
+			sendErrorResponse(responder, MeetingResponseStatus.INVALID_MEETING_RREQUEST);
 		} catch (ProcessingEmailException e) {
-			sendErrorResponse(responder, MeetingResponseStatus.SERVER_ERROR, e);
+			logger.error(e.getMessage(), e);
+			sendErrorResponse(responder, MeetingResponseStatus.SERVER_ERROR);
 		}
 	}
 	
-	private void sendErrorResponse(Responder responder, MeetingResponseStatus status, Exception exception) {
-		logger.error(exception.getMessage(), exception);
+	private void sendErrorResponse(Responder responder, MeetingResponseStatus status) {
 		sendResponse(responder, meetingProtocol.encodeErrorResponse(status));
 	}
 	
@@ -128,8 +129,10 @@ public class MeetingResponseHandler extends WbxmlRequestHandler {
 					meetingResponse.setCalId(calId);
 				}
 			} catch (ServerItemNotFoundException e) {
+				logger.error(e.getMessage(), e);
 				meetingResponse.setStatus(MeetingResponseStatus.SERVER_ERROR);
 			} catch (UnknownObmSyncServerException e) {
+				logger.error(e.getMessage(), e);
 				meetingResponse.setStatus(MeetingResponseStatus.SERVER_ERROR);
 			}
 		} else {
