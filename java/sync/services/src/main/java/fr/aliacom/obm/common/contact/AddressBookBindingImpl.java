7a256be3dd0e4ec51bfbc8f1b9811ced8cf548aa
Joakim Ribier <jribier@linagora.com>
2012-01-04 15:55:15 +0100

    - [OBMFULL-3014] fix bug on contact deletion

--- java/sync/services/src/main/java/fr/aliacom/obm/common/contact/AddressBookBindingImpl.java
+++ java/sync/services/src/main/java/fr/aliacom/obm/common/contact/AddressBookBindingImpl.java
@@ -21,7 +21,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -360,7 +359,7 @@ public class AddressBookBindingImpl implements IAddressBook {
 	@Transactional
 	public ContactChanges listContactsChanged(AccessToken token, Date lastSync, Integer addressBookId) throws ServerFault {
 		try {
-			Set<Integer> removal = new HashSet<Integer>();
+			Set<Integer> removal = null;
 			ContactUpdates contactUpdates = null;
 			
 			if (addressBookId == contactConfiguration.getAddressBookUserId()) {
@@ -371,13 +370,16 @@ public class AddressBookBindingImpl implements IAddressBook {
 				removal = contactDao.findRemovalCandidates(lastSync, addressBookId, token);
 			}
 			
-			return new ContactChanges(contactUpdates.getContacts(), removal, getLastSync());
-	
+			return new ContactChanges(
+					contactUpdates.getContacts(), 
+					Sets.union(contactUpdates.getArchived(), removal), 
+					getLastSync());
+			
 		} catch (SQLException ex) {
 			throw new ServerFault(ex);
 		}
 	}
-	
+		
 	private Date getLastSync() throws ServerFault {
 		Connection connection = null;
 		try {
