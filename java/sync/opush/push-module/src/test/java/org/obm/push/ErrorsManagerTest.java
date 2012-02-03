03f8574094c9bb494d1803df492367673e2c5e60
Joakim Ribier <jribier@linagora.com>
2012-02-03 17:46:30 +0100

    - [OBMFULL-3141] CRU-904

--- java/sync/opush/push-module/src/test/java/org/obm/push/ErrorsManagerTest.java
+++ java/sync/opush/push-module/src/test/java/org/obm/push/ErrorsManagerTest.java
@@ -2,6 +2,8 @@ package org.obm.push;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.james.mime4j.dom.Message;
 import org.apache.james.mime4j.field.address.ParseException;
@@ -13,11 +15,14 @@ import org.obm.push.bean.Credentials;
 import org.obm.push.bean.User;
 import org.obm.push.utils.Mime4jUtils;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+
 public class ErrorsManagerTest {
 
 	@Test
 	public void testPrepareMessage() throws ParseException, FileNotFoundException, IOException {
-		User user = User.Factory.create().createUser("test@test", "test@domain", "displayName");
+		User user = User.Factory.create().createUser("test@domain", "test@domain", "displayName");
 		BackendSession backendSession = new BackendSession(new Credentials(user, "password", null), null, null, null);
 		
 		Mime4jUtils mime4jUtils = new Mime4jUtils();
@@ -25,9 +30,17 @@ public class ErrorsManagerTest {
 		ErrorsManager errorsManager = new ErrorsManager(null, null, mime4jUtils);
 		Message message = errorsManager.prepareMessage(backendSession, "Subject", "Body", 
 				StreamMailTestsUtils.newInputStreamFromString("It's mail content !"));
-		mime4jUtils.toInputStream(message);
-
+		
+		InputStream stream = mime4jUtils.toInputStream(message);
+		String messageAsString = CharStreams.toString(new InputStreamReader(stream, Charsets.UTF_8));
+		
 		Assertions.assertThat(message).isNotNull();
+		Assertions.assertThat(message.isMultipart()).isTrue();
+		Assertions.assertThat(messageAsString).contains("test@domain")
+					.contains("Subject: Subject")
+					.contains("Body")
+					.contains("Content-Disposition: attachment; filename=error_message.eml")
+					.contains("It's mail content !");
 	}
 
 }
